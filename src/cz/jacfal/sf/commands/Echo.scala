b7f8171606e0aca6dee02954ab6fd59407dbda5f
Jacob True <jacfal.tech@protonmail.com>
2019-11-18 21:29:44 +0100

    Echo command updated

--- src/cz/jacfal/sf/commands/Echo.scala
+++ src/cz/jacfal/sf/commands/Echo.scala
@@ -1,4 +1,5 @@
 package cz.jacfal.sf.commands
+import cz.jacfal.sf.files.{DirEntry, Directory, File}
 import cz.jacfal.sf.filesystem.State
 
 import scala.annotation.tailrec
@@ -35,7 +36,42 @@ class Echo(args: Array[String]) extends Command {
     createContentHelper(0, "")
   }
 
+  def getRootAfterEcho(currentDirectory: Directory, path: List[String], contents: String, append: Boolean): Directory = {
+    if (path.isEmpty) currentDirectory
+    else if (path.tail.isEmpty) {
+      val dirEntry = currentDirectory.findEntry(path.head)
+
+      if (dirEntry == null) {
+        currentDirectory.addEntry(new File(currentDirectory.path, path.head, contents))
+      } else if (dirEntry.isDir) {
+        currentDirectory
+      } else {
+        if (append)
+          currentDirectory.replaceEntry(path.head, dirEntry.asFile.appendContents(contents))
+        else
+          currentDirectory.replaceEntry(path.head, dirEntry.asFile.setContents(contents))
+      }
+    } else {
+      val nextEntry: Directory = currentDirectory.findEntry(path.head).asDirectory
+      val newNextEntry: Directory = getRootAfterEcho(nextEntry, path.tail, contents, append)
+
+      if (newNextEntry == nextEntry)
+        currentDirectory
+      else
+        currentDirectory.replaceEntry(path.head, newNextEntry)
+    }
+  }
+
   def doEcho(state: State, content: String, filename: String, appendMode: Boolean): State = {
-    ???
+    if (filename.contains(Directory.SEPARATOR)) {
+      state.setMessage("Echo: filename must not contains separators")
+    } else {
+      val newRoot: Directory = getRootAfterEcho(state.root, state.workingDirectory.getAllFoldersInThePath :+ filename, content, appendMode)r
+      if (newRoot == state.root) {
+        state.setMessage(filename + ": no such file")
+      } else {
+        State(newRoot, newRoot.findDescendant(state.workingDirectory.getAllFoldersInThePath))
+      }
+    }
   }
 }
